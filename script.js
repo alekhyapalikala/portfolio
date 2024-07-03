281db6d02487a6322a12c07bf6ded0972ce7c1f3
venugopal <pailavenugopal2002@gmail.com>
2024-07-03 12:42:43 +0530

    new

---
+++ script.js
@@ -0,0 +1,75 @@
+let filterItem = document.querySelector('.items-links');
+let fileteImages = document.querySelectorAll('.project-img');
+
+
+
+
+window.addEventListener('load', () => {
+    filterItem.addEventListener('click', (selectedItem) => {
+        if (selectedItem.target.classList.contains('item-link')) {
+            document.querySelector('.menu-active').classList.remove('menu-active');
+            selectedItem.target.classList.add('menu-active');
+            let filterName = selectedItem.target.getAttribute('data-name');
+            fileteImages.forEach((image) => {
+                let filterImages = image.getAttribute('data-name');
+                if ((filterImages == filterName) || filterName == 'all') {
+                    image.style.display = 'block'
+
+                } else {
+                    image.style.display = 'none'
+                }
+
+            })
+        }
+
+    })
+})
+
+
+
+
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
